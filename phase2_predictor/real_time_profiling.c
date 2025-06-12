d8fa48a1399282a4c2a6e7e4fe6ce2661203d8de
vp722 <vp722@ic.ac.uk>
2025-06-12 12:47:34 +0100

    fix : weight

--- phase2_predictor/real_time_profiling.c
+++ phase2_predictor/real_time_profiling.c
@@ -475,7 +475,7 @@ void run_executable(const char *program, char *const argv[]) {
         counts[COUNTER_COUNT] = {0};
         double avg_deltas[COUNTER_COUNT] = {0};
         // asign linear weights to the sliding window
-        double weights[SLIDING_WINDOW] = {1,2,3,4,5, 6, 7, 8, 9, 10, 11,12, 13,14,15,16,17,18,19,10}; // weights for the sliding window
+        double weights[SLIDING_WINDOW] = {1,2,3,4,5, 6,7, 8, 9, 10, 11,12, 13,14,15,16,17,18,19,20}; // weights for the sliding window
 	    uint64_t total_sampling_ns = 0;
 	    int num_samples = 0;
 
