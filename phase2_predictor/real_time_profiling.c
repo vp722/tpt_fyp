c8ffe7ce29785d7acabd9df55d45d9240ace9fa0
vp722 <vp722@ic.ac.uk>
2025-06-12 12:13:16 +0100

    fix : div by 0

--- phase2_predictor/real_time_profiling.c
+++ phase2_predictor/real_time_profiling.c
@@ -326,7 +326,7 @@ bool should_enable_tpt_sliding_window(double avg_deltas[], pid_t pid, int counts
     double memory_accesses = avg_deltas[2] + avg_deltas[3]; // load and store misses
     // double avg_ept_walk_per_miss  = avg_deltas[4] / walks_completed;
     // double avg_walk_cycles_per_miss = (avg_deltas[2] + avg_deltas[3] + avg_deltas[6]) / walks_completed;
-    double ept_cycles_per_execution_cycles = avg_deltas[4] / avg_deltas[0];
+    double ept_cycles_per_execution_cycles = (avg_deltas[0] != 0) ? (avg_deltas[4] / avg_deltas[0]) : 0.0;
     double rss = get_rss_in_bytes(pid);
     double rss_in_gb = (double)rss / (1024 * 1024 * 1024); // Convert bytes to GB
     
